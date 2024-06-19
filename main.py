c6d841657b16feda473ab9c0055663f90500503f
bloombar <2209098+bloombar@users.noreply.github.com>
2024-06-18 22:08:51 -0400

    easy done

---
+++ main.py
@@ -0,0 +1,29 @@
+import easy
+import medium
+import difficult
+
+def do_easy():
+    # "easy" problems
+    easy.one()
+    easy.two()
+    easy.three()
+    easy.four()
+    easy.five(10)
+    easy.six(8)
+    easy.seven('circumlocution')
+    easy.eight(24)
+    easy.nine()
+    easy.ten('circumlocution')
+
+def do_medium():
+    pass # do nothing
+
+def do_difficult():
+    pass # do nothing
+
+def main():
+    do_easy()
+    do_medium()
+    do_difficult()
+
+main()
